c6d6a0cb396a900929d07a70399425aae859290c
sub-prime <sub-pr1me9000@gmail.com>
2026-03-09 08:24:35 +0200

    Add AuctionsMonitor component

--- client/src/components/Auctions.jsx
+++ client/src/components/Auctions.jsx
@@ -1,10 +1,12 @@
 import styles from '../styles/Auctions.module.css'
 import useAuth from '../hooks/useAuth'
-import { useEffect, useEffectEvent } from 'react'
+import { useState, useEffect, useEffectEvent } from 'react'
 import SetAuctions from './SetAuctions'
+import AuctionsMonitor from './AuctionsMonitor'
 
 const Auctions = ({ setActive }) => {
   
+  const [section, setSection] = useState('current');
   const { auth } = useAuth();
   const onRefresh = useEffectEvent(()=>{setActive('auctions')});
   
@@ -16,7 +18,13 @@ const Auctions = ({ setActive }) => {
     <>
     <title>Auctions</title>
     <div className={`${styles.auctions_container}`}>
-      {auth.roles[0] === 'venue' && <SetAuctions />}
+      <AuctionsMonitor
+        section={section}
+        setSection={setSection}
+      />
+      <div className={`${styles.tables}`}>
+        {auth.roles[0] === 'venue' && <SetAuctions />}
+      </div>
     </div>
     </>
   );
---
+++ client/src/components/AuctionsMonitor.jsx
@@ -0,0 +1,21 @@
+import styles from '../styles/AuctionsMonitor.module.css'
+
+const AuctionsMonitor = ({ section, setSection }) => {
+  return (
+    <>
+    <div className={`${styles.monitor}`}>
+        <div className={`${styles.navigate}`}>
+          <div className={`${styles.current}`} onClick={()=>{setSection('current')}}>Current</div>
+          <div className={`${styles.history}`} onClick={()=>{setSection('history')}}>History</div>
+        </div>
+        <div className={`${styles.line} ${section === 'current' ? styles.left : styles.right}`}></div>
+        <div className={`${styles.sections} ${section !== 'current' ? styles.curr : styles.hist}`}>
+          <div className={`${styles.history_section}`}>History</div>
+          <div className={`${styles.current_section}`}>Current</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AuctionsMonitor
