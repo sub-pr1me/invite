4e509a72a351546b46d96c95b310f7ea834c593e
sub-prime <sub-pr1me9000@gmail.com>
2026-05-20 18:39:26 +0300

    Add Prisma ORM

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,53 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" SERIAL NOT NULL,
+    "customer" VARCHAR(255),
+    "email" CITEXT NOT NULL,
+    "password" VARCHAR(255),
+    "stage" VARCHAR(255),
+    "avatar" VARCHAR(255),
+    "album" TEXT[],
+    "likes" TEXT[],
+    "dob" VARCHAR(255),
+    "age" VARCHAR(255),
+    "gender" VARCHAR(255),
+    "interest" VARCHAR(255),
+    "dates" JSONB,
+    "credits" DECIMAL(4,0),
+    "reftoken" VARCHAR(255),
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "venues" (
+    "id" SERIAL NOT NULL,
+    "venue" VARCHAR(255) NOT NULL,
+    "email" CITEXT NOT NULL,
+    "password" VARCHAR(255),
+    "stage" VARCHAR(255),
+    "avatar" VARCHAR(255),
+    "album" TEXT[],
+    "likes" TEXT[],
+    "rating" DECIMAL(2,1),
+    "hours" VARCHAR(255),
+    "tables" JSONB,
+    "auctions" JSONB,
+    "dates" JSONB,
+    "credits" DECIMAL(4,0),
+    "reftoken" VARCHAR(255),
+
+    CONSTRAINT "venues_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "customers_email_key" ON "customers"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "venues_venue_key" ON "venues"("venue");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "venues_email_key" ON "venues"("email");
