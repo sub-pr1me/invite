c8690cd47ecc110033ab31999edf50f91ab61987
sub-prime <sub-pr1me9000@gmail.com>
2026-05-27 15:30:07 +0300

    fix: add railway frontend url in allowedHosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: true
+    host: true,
+    allowedHosts: ["frontend-production-e5749.up.railway.app"]
   },
   
   plugins: [react()],
