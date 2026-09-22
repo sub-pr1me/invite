95abbbd6e64b46b418e57f1ea9bfd97ece4389c6
sub-pr1me <122296954+sub-pr1me@users.noreply.github.com>
2026-09-22 20:43:25 +0300

    chore: migrate AuthProvider context to TS

---
+++ client/src/context/AuthProvider.tsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react"
+import { AuthContextType, AuthType, CustomizeType, AuthProviderProps } from "../types"
+
+const AuthContext = createContext({} as AuthContextType);
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [auth, setAuth] = useState<AuthType>(null);
+  const [activeEmail, setActiveEmail] = useState<string | null>(null);
+  const [active, setActive] = useState('home');
+  const [customize, setCustomize] = useState<CustomizeType>(null);
+
+  return (
+    <AuthContext.Provider value={{
+      auth, setAuth, activeEmail, setActiveEmail,
+      active, setActive, customize, setCustomize }}
+    >{children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
